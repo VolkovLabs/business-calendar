e31e7edb31add7495e510ae91d79458abba2a21b
Marcus Olsson <marcus.olsson@hey.com>
2021-02-11 09:55:57 +0100

    Add plugin fallback

--- src/module.ts
+++ src/module.ts
@@ -1,30 +1,33 @@
 import { FieldType, PanelPlugin } from '@grafana/data';
 import { CalendarOptions } from './types';
 import { CalendarPanel } from './CalendarPanel';
-import { FieldSelectEditor } from 'grafana-plugin-support';
+import { FieldSelectEditor, getPanelPluginOrFallback } from 'grafana-plugin-support';
 
-export const plugin = new PanelPlugin<CalendarOptions>(CalendarPanel).setNoPadding().setPanelOptions(builder => {
-  return builder
-    .addCustomEditor({
-      id: 'timeField',
-      path: 'timeField',
-      name: 'Time',
-      description: 'Field to use for the time. Defaults to the first time field.',
-      editor: FieldSelectEditor,
-      category: ['Dimensions'],
-      settings: {
-        filterByType: [FieldType.time],
-      },
-    })
-    .addCustomEditor({
-      id: 'textField',
-      path: 'textField',
-      name: 'Text',
-      description: 'Field to use for the text. Defaults to the first textual field.',
-      editor: FieldSelectEditor,
-      category: ['Dimensions'],
-      settings: {
-        filterByType: [FieldType.string],
-      },
-    });
-});
+export const plugin = getPanelPluginOrFallback(
+  'marcusolsson-calendar-panel',
+  new PanelPlugin<CalendarOptions>(CalendarPanel).setNoPadding().setPanelOptions(builder => {
+    return builder
+      .addCustomEditor({
+        id: 'timeField',
+        path: 'timeField',
+        name: 'Time',
+        description: 'Field to use for the time. Defaults to the first time field.',
+        editor: FieldSelectEditor,
+        category: ['Dimensions'],
+        settings: {
+          filterByType: [FieldType.time],
+        },
+      })
+      .addCustomEditor({
+        id: 'textField',
+        path: 'textField',
+        name: 'Text',
+        description: 'Field to use for the text. Defaults to the first textual field.',
+        editor: FieldSelectEditor,
+        category: ['Dimensions'],
+        settings: {
+          filterByType: [FieldType.string],
+        },
+      });
+  })
+);

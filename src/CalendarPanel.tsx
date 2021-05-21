fbe01c421b20b7624f887912cda9e5d53e461245
Marcus Olsson <marcus.olsson@hey.com>
2021-05-21 10:43:54 +0200

    Fix colors for earlier Grafana versions
    Fixes #16

--- src/CalendarPanel.tsx
+++ src/CalendarPanel.tsx
@@ -54,6 +54,10 @@ export const CalendarPanel: React.FC<Props> = ({
   const endOfWeek = to.endOf('isoWeek');
   const numDays = endOfWeek.diff(startOfWeek, 'days');
 
+  const colors = classicColors ?? legacyClassicColors;
+
+  console.log(colors[0]);
+
   const events = frames.flatMap((frame, frameIdx) => {
     return frame.text && frame.start
       ? Array.from({ length: frame.text.values.length })
@@ -69,7 +73,7 @@ export const CalendarPanel: React.FC<Props> = ({
             description,
             labels,
             start: dayjs(start),
-            color: classicColors[Math.floor(frameIdx % classicColors.length)],
+            color: colors[Math.floor(frameIdx % classicColors.length)],
 
             // Set undefined if the user hasn't explicitly configured the dimension
             // for end time.
@@ -203,3 +207,63 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => {
     `,
   };
 });
+
+// For Grafana versions older than 7.3.0.
+export const legacyClassicColors = [
+  '#7EB26D', // 0: pale green
+  '#EAB839', // 1: mustard
+  '#6ED0E0', // 2: light blue
+  '#EF843C', // 3: orange
+  '#E24D42', // 4: red
+  '#1F78C1', // 5: ocean
+  '#BA43A9', // 6: purple
+  '#705DA0', // 7: violet
+  '#508642', // 8: dark green
+  '#CCA300', // 9: dark sand
+  '#447EBC',
+  '#C15C17',
+  '#890F02',
+  '#0A437C',
+  '#6D1F62',
+  '#584477',
+  '#B7DBAB',
+  '#F4D598',
+  '#70DBED',
+  '#F9BA8F',
+  '#F29191',
+  '#82B5D8',
+  '#E5A8E2',
+  '#AEA2E0',
+  '#629E51',
+  '#E5AC0E',
+  '#64B0C8',
+  '#E0752D',
+  '#BF1B00',
+  '#0A50A1',
+  '#962D82',
+  '#614D93',
+  '#9AC48A',
+  '#F2C96D',
+  '#65C5DB',
+  '#F9934E',
+  '#EA6460',
+  '#5195CE',
+  '#D683CE',
+  '#806EB7',
+  '#3F6833',
+  '#967302',
+  '#2F575E',
+  '#99440A',
+  '#58140C',
+  '#052B51',
+  '#511749',
+  '#3F2B5B',
+  '#E0F9D7',
+  '#FCEACA',
+  '#CFFAFF',
+  '#F9E2D2',
+  '#FCE2DE',
+  '#BADFF4',
+  '#F9D9F9',
+  '#DEDAF7',
+];

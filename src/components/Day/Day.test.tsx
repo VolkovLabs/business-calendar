e880900a2b88a0c65e7d06e6af3898551fbe3c4c
Mikhail <mikhail@volkovlabs.io>
2023-02-22 10:08:02 -0500

    Add Day test

---
+++ src/components/Day/Day.test.tsx
@@ -0,0 +1,19 @@
+import dayjs from 'dayjs';
+import { shallow } from 'enzyme';
+import React from 'react';
+import { Day } from './Day';
+
+/**
+ * Day
+ */
+describe('Day', () => {
+  it('Should find component', async () => {
+    const getComponent = ({ ...restProps }: any) => {
+      return <Day {...restProps} />;
+    };
+
+    const wrapper = shallow(getComponent({ day: dayjs(Date.now()), events: [] }));
+    const div = wrapper.find('div');
+    expect(div.exists()).toBeTruthy();
+  });
+});

e7416068f1171141d70b6fbdbdfb30227546c5dc
Mikhail <mikhail@volkovlabs.io>
2023-03-05 09:39:39 -0500

    Refactor interval

--- src/utils/alignEvents.ts
+++ src/utils/alignEvents.ts
@@ -1,4 +1,4 @@
-import { OpUnitType } from 'dayjs';
+import dayjs, { OpUnitType } from 'dayjs';
 import { CalendarEvent } from '../types';
 
 /**
@@ -25,47 +25,55 @@ export const alignEvents = (
     /**
      * Single day event
      */
-    let interval = [{ day: event.start.format('YYYY-MM-DD'), event }];
+    if (!event.end) {
+      const interval = [{ day: event.start.format('YYYY-MM-DD'), event }];
+      return addEvents(event.start, alignedEvents, interval);
+    }
 
     /**
      * Multi-Day
      */
-    if (event.end) {
-      let duration = event.end.endOf('day').diff(event.start.startOf('day'), 'days') + 1;
+    const duration = event.end.endOf('day').diff(event.start.startOf('day'), 'days') + 1;
 
-      interval = Array.from({ length: duration })
-        .map((_, i) => event.start.add(i, 'days'))
-        .map((d) => ({ day: d.format('YYYY-MM-DD'), event }));
-    }
+    const interval = Array.from({ length: duration })
+      .map((_, i) => event.start.add(i, 'days'))
+      .map((d) => ({ day: d.format('YYYY-MM-DD'), event }));
 
-    /**
-     * Offset determines the vertical position of the event. It's used to make
-     * sure the entries are vertically aligned.
-     */
-    const eventsOnStart = alignedEvents[event.start.format('YYYY-MM-DD')];
-    let offset = 0;
-    if (eventsOnStart) {
-      /**
-       * Find the first available vertical slot, or add it to the end.
-       */
-      const firstAvailableIndex = eventsOnStart.findIndex((event) => !event);
-      offset = firstAvailableIndex < 0 ? eventsOnStart.length : firstAvailableIndex;
-    }
-
-    /**
-     * We expand each event to an entry for each day it spans.
-     */
-    interval.forEach((entry) => {
-      if (!alignedEvents[entry.day]) {
-        alignedEvents[entry.day] = [];
-      }
-
-      while (alignedEvents[entry.day].length < offset) {
-        alignedEvents[entry.day].push(undefined as any);
-      }
-      alignedEvents[entry.day].splice(offset, 1, entry.event);
-    });
+    addEvents(event.start, alignedEvents, interval);
   });
 
   return alignedEvents;
+};
+
+/**
+ * Add Events starting from the Start day
+ */
+const addEvents = (start: dayjs.Dayjs, alignedEvents: Record<string, CalendarEvent[]>, interval: any) => {
+  /**
+   * Offset determines the vertical position of the event. It's used to make
+   * sure the entries are vertically aligned.
+   */
+  const eventsOnStart = alignedEvents[start.format('YYYY-MM-DD')];
+  let offset = 0;
+  if (eventsOnStart) {
+    /**
+     * Find the first available vertical slot, or add it to the end.
+     */
+    const firstAvailableIndex = eventsOnStart.findIndex((event) => !event);
+    offset = firstAvailableIndex < 0 ? eventsOnStart.length : firstAvailableIndex;
+  }
+
+  /**
+   * We expand each event to an entry for each day it spans.
+   */
+  interval.forEach((entry: any) => {
+    if (!alignedEvents[entry.day]) {
+      alignedEvents[entry.day] = [];
+    }
+
+    while (alignedEvents[entry.day].length < offset) {
+      alignedEvents[entry.day].push(undefined as any);
+    }
+    alignedEvents[entry.day].splice(offset, 1, entry.event);
+  });
 };

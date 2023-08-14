758ec949881103638eaca0dddf9ed3d2a7b1f460
asimonok <sashasimonok@gmail.com>
2023-08-14 11:11:51 +0300

    Add tests for localize messages

--- src/hooks/useLocalizer.test.ts
+++ src/hooks/useLocalizer.test.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import { config } from '@grafana/runtime';
 import { renderHook } from '@testing-library/react';
+import { LanguageMessages } from '../constants';
 import { useLocalizer } from './useLocalizer';
 
 /**
@@ -47,4 +48,23 @@ describe('Use Localizer', () => {
 
     expect(dayjs.locale()).toEqual('en');
   });
+
+  ['es-SP', 'fr-EN', 'de-GE', 'zh-CH'].forEach((lang) => {
+    it(`Should set messages for ${lang}`, () => {
+      config.bootData = {
+        user: {
+          language: lang,
+        },
+      } as any;
+      const { result } = renderHook(() => useLocalizer());
+
+      const locale = lang.split('-')[0]
+      const expectedMessages = LanguageMessages[locale];
+      expect(result.current.messages).toEqual(expectedMessages)
+      if (result.current.messages.showMore && expectedMessages.showMore) {
+        expect(result.current.messages.showMore(10)).toEqual(expectedMessages.showMore(10))
+      }
+    });
+  })
+
 });

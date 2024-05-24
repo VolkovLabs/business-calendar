765b0cbe7c450b023bdfae59125e49d374f396a7
vitPinchuk <vit.pinchuck@gmail.com>
2024-05-24 17:54:37 +0300

    update format and view

--- src/components/BigEventContent/BigEventContent.test.tsx
+++ src/components/BigEventContent/BigEventContent.test.tsx
@@ -1,34 +1,150 @@
 import { render, screen } from '@testing-library/react';
+import { getJestSelectors } from '@volkovlabs/jest-selectors';
 import dayjs from 'dayjs';
 import React from 'react';
 import { dayjsLocalizer } from 'react-big-calendar';
 
+import { TEST_IDS } from '../../constants';
 import { BigEventContent } from './BigEventContent';
+
+/**
+ * Component Props
+ */
+type Props = React.ComponentProps<typeof BigEventContent>;
 
 describe('Big Event Content', () => {
   const localizer = dayjsLocalizer(dayjs);
 
-  it('Should render time', () => {
-    render(
-      <BigEventContent
-        event={{ title: '123', resource: {}, start: new Date('2020-02-02 02:00') }}
-        localizer={localizer as any}
-        showTime={true}
-      />
-    );
+  /**
+   * Selectors
+   */
+  const getSelectors = getJestSelectors(TEST_IDS.eventContent);
+  const selectors = getSelectors(screen);
 
-    expect(screen.getByText('')).toContainHTML('2:00 AM');
+  /**
+   * Default Event
+   */
+  const defaultEvent = {
+    title: '123',
+    start: new Date('2020-02-02 02:00'),
+    end: new Date('2020-02-02 02:30'),
+    resource: { location: 'Room' },
+  };
+
+  /**
+   * Default Options
+   */
+  const defaultOptions = {
+    event: defaultEvent,
+    localizer: localizer as any,
+  } as any;
+
+  /**
+   * Get Tested Component
+   * @param props
+   */
+  const getComponent = (props: Partial<Props>) => {
+    return <BigEventContent {...(props as any)} />;
+  };
+
+  it('Should render time', () => {
+    render(getComponent(defaultOptions));
+
+    expect(screen.getByText('2:00 AM')).toBeInTheDocument();
   });
 
   it('Should render title', () => {
-    render(<BigEventContent event={{ title: '123', resource: {} }} localizer={localizer as any} />);
+    render(getComponent(defaultOptions));
 
     expect(screen.getByText('123')).toBeInTheDocument();
   });
 
   it('Should render location', () => {
-    render(<BigEventContent event={{ title: '123', resource: { location: 'Room' } }} localizer={localizer as any} />);
+    render(getComponent(defaultOptions));
 
-    expect(screen.getByText(': Room')).toBeInTheDocument();
+    expect(screen.getByText('123')).toBeInTheDocument();
+
+    expect(screen.getByText('Room')).toBeInTheDocument();
+  });
+
+  it('Should render Month view', () => {
+    render(
+      getComponent({
+        ...defaultOptions,
+        isMonth: true,
+      })
+    );
+
+    expect(screen.getByText('123')).toBeInTheDocument();
+    expect(selectors.month()).toBeInTheDocument();
+  });
+
+  it('Should render Agenda view', () => {
+    render(
+      getComponent({
+        ...defaultOptions,
+        isAgenda: true,
+      })
+    );
+
+    expect(screen.getByText('123')).toBeInTheDocument();
+    expect(selectors.agenda()).toBeInTheDocument();
+  });
+
+  it('Should render view for duration more or equal 90 minutes', () => {
+    render(
+      getComponent({
+        ...defaultOptions,
+        event: {
+          ...defaultEvent,
+          end: new Date('2020-02-02 04:30'),
+        },
+      })
+    );
+
+    expect(screen.getByText('123')).toBeInTheDocument();
+    expect(screen.getByText('2:00 AM – 4:30 AM')).toBeInTheDocument();
+
+    expect(selectors.longDuration()).toBeInTheDocument();
+  });
+
+  it('Should render view for duration more or equal 45 minutes', () => {
+    render(
+      getComponent({
+        ...defaultOptions,
+        event: {
+          ...defaultEvent,
+          end: new Date('2020-02-02 02:50'),
+        },
+      })
+    );
+
+    expect(screen.getByText('123')).toBeInTheDocument();
+    expect(screen.getByText('2:00 AM')).toBeInTheDocument();
+
+    expect(selectors.averageDuration()).toBeInTheDocument();
+  });
+
+  it('Should render view if event doesn`t have end time', () => {
+    render(
+      getComponent({
+        ...defaultOptions,
+        event: {
+          ...defaultEvent,
+          end: new Date('2020-02-02 02:50'),
+          resource: {
+            noEndTime: true,
+          },
+        },
+      })
+    );
+
+    expect(screen.getByText('123')).toBeInTheDocument();
+    expect(screen.getByText('2:00 AM')).toBeInTheDocument();
+
+    expect(selectors.averageDuration(true)).not.toBeInTheDocument();
+    expect(selectors.longDuration(true)).not.toBeInTheDocument();
+    expect(selectors.month(true)).not.toBeInTheDocument();
+    expect(selectors.agenda(true)).not.toBeInTheDocument();
   });
 });

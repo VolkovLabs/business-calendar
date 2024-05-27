94c99e4c83484cb35b8902d7a2903578d410ccd1
Mikhail Volkov <mikhail@volkovlabs.io>
2024-05-26 23:12:27 -0400

    Update panel options

--- src/constants/options.ts
+++ src/constants/options.ts
@@ -72,6 +72,6 @@ export const DISPLAY_FIELD_OPTIONS = (t: TFunction) => [
  * Preformatted Options
  */
 export const PREFORMATTED_OPTIONS = (t: TFunction) => [
-  { value: false, label: t('panelOptions.events.preformattedOptions.options.default'), icon: 'align-left' },
+  { value: false, label: t('panelOptions.events.preformattedOptions.options.default'), icon: 'text-fields' },
   { value: true, label: t('panelOptions.events.preformattedOptions.options.preformatted'), icon: 'bars' },
 ];

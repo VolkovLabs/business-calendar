4d94017cf5798c7f5e2778251089704b02d6c966
asimonok <sashasimonok@gmail.com>
2024-07-16 08:11:03 +0300

    Fix type errors

--- src/components/DateEditor/DateEditor.tsx
+++ src/components/DateEditor/DateEditor.tsx
@@ -18,8 +18,8 @@ export const DateEditor: React.FC<Props> = ({ value, onChange }) => {
       minDate={undefined}
       maxDate={undefined}
       date={dateTime(value)}
-      onChange={(dateTime: DateTime) => {
-        onChange(dateTime.toISOString());
+      onChange={(dateTime?: DateTime) => {
+        onChange(dateTime?.toISOString());
       }}
       data-testid={TEST_IDS.dateEditor.field}
     />

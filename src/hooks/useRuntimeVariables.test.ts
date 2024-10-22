577a8055e2437b3c232e0d466ebff14131a87cec
vitPinchuk <vit.pinchuck@gmail.com>
2024-10-22 17:56:36 +0300

    update useRuntimeVariables hook to use in dashboard scene

--- src/hooks/useRuntimeVariables.test.ts
+++ src/hooks/useRuntimeVariables.test.ts
@@ -1,5 +1,5 @@
-import { getTemplateSrv } from '@grafana/runtime';
-import { act, renderHook, waitFor } from '@testing-library/react';
+import { CustomVariableModel, EventBusSrv, LoadingState, TypedVariableModel, VariableHide } from '@grafana/data';
+import { renderHook } from '@testing-library/react';
 
 import { VariableType } from '../types';
 import { useRuntimeVariables } from './useRuntimeVariables';
@@ -7,114 +7,54 @@ import { useRuntimeVariables } from './useRuntimeVariables';
 /**
  * Mock @grafana/runtime
  */
+const getVariablesMock = jest.fn((): TypedVariableModel[] => []);
+
 jest.mock('@grafana/runtime', () => ({
+  ...jest.requireActual('@grafana/runtime'),
   getTemplateSrv: jest.fn(() => ({
-    getVariables: jest.fn(() => []),
+    getVariables: getVariablesMock,
   })),
 }));
 
-/**
- * Create eventBus mock
- */
-const createEventBus = () => {
-  let subscriber: () => void;
+describe('useRuntimeVariables', () => {
+  /**
+   * Event Bus
+   */
+  const eventBus = new EventBusSrv();
 
-  return {
-    getStream: jest.fn(() => ({
-      subscribe: jest.fn((handler) => {
-        subscriber = handler;
-        return {
-          unsubscribe: jest.fn(),
-        };
-      }),
-    })),
-    publish: () => {
-      if (subscriber) {
-        subscriber();
-      }
-    },
-  };
-};
-
-describe('Use Runtime Variables', () => {
-  const eventBus: any = createEventBus();
-
-  const variableDevice = {
-    name: 'device',
+  /**
+   * Create Variable
+   */
+  const createVariable = (item: Partial<CustomVariableModel>): CustomVariableModel => ({
     type: VariableType.CUSTOM,
-    options: [
-      {
-        text: 'Device1',
-        value: '1',
-      },
-    ],
-  };
-  const variableCountry = {
-    name: 'country',
-    type: VariableType.QUERY,
+    name: '',
     options: [],
-  };
-  const variableText = {
-    name: 'text',
-    type: VariableType.TEXTBOX,
-    options: [],
-  };
-  const variableConstant = {
-    name: 'constant',
-    type: VariableType.CONSTANT,
-    options: [],
-  };
-  const variableData = {
-    name: 'data',
-    type: 'datasource',
-    options: [],
-  };
-  jest.mocked(getTemplateSrv).mockImplementation(
-    () =>
-      ({
-        getVariables: jest.fn(() => [variableCountry, variableDevice, variableData, variableText, variableConstant]),
-      }) as any
-  );
-
-  it('Should return variable', () => {
-    const { result } = renderHook(() => useRuntimeVariables(eventBus, 'device'));
-
-    expect(result.current.variable).toEqual({
-      ...variableDevice,
-    });
+    id: '',
+    multi: false,
+    includeAll: false,
+    current: {},
+    query: '',
+    rootStateKey: '',
+    global: false,
+    hide: VariableHide.dontHide,
+    description: '',
+    allValue: null,
+    skipUrlSync: false,
+    index: 0,
+    state: LoadingState.Done,
+    error: '',
+    ...item,
   });
 
-  it('Should not return variable', () => {
-    const { result } = renderHook(() => useRuntimeVariables(eventBus, 'data'));
+  it('Should return variables', () => {
+    getVariablesMock.mockReturnValue([createVariable({ name: 'device' })]);
 
-    expect(result.current.variable).toBeUndefined();
-  });
-
-  it('Should update variable', async () => {
     const { result } = renderHook(() => useRuntimeVariables(eventBus, 'device'));
 
-    expect(result.current.variable).toEqual({
-      ...variableDevice,
-    });
-
-    /**
-     * Update variables
-     */
-    jest.mocked(getTemplateSrv).mockImplementation(
-      () =>
-        ({
-          getVariables: jest.fn(() => []),
-        }) as any
+    expect(result.current.getVariable('device')).toEqual(
+      expect.objectContaining({
+        name: 'device',
+      })
     );
-
-    /**
-     * Trigger refresh event
-     */
-    await act(() => eventBus.publish());
-
-    /**
-     * Check if updated variable returns
-     */
-    await waitFor(() => expect(result.current.variable).not.toBeDefined());
   });
 });
